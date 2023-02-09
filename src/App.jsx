d0341fec64ce9f2a3c835f93dae1c744d4f490e2
Jahid Eljah <jahideljah@gmail.com>
2023-02-09 23:56:46 +0100

    multiple-returns-fetch-data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Starter from "./tutorial/02-useEffect/starter/04-fetch-data";
-// import Final from "./tutorial/01-useState/final/01-error-example";
+// import Starter from "./tutorial/02-useEffect/starter/04-fetch-data";
+// import Starter from "./tutorial/03-conditional-rendering/starter/01-multiple-returns-basics";
+import Starter from "./tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data";
+import Final from "./tutorial/03-conditional-rendering/final/02-multiple-returns-fetch-data";
 
 function App() {
   return (
     <div className="container">
       <Starter />
-      {/* <Final /> */}
+      {/*   <Final /> */}
     </div>
   );
 }
--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,45 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from "react";
+
+const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+  const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+        setUser(data);
+      } catch (err) {
+        console.log(err);
+        setIsError(true);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <h4>Loading ...</h4>;
+  }
+  if (isError) {
+    return <h4> Thers is an Error</h4>;
+  }
+
+  return (
+    <div>
+      <h2>Fetch Data</h2>
+      <div>
+        <img src={user.avatar_url} alt="img" />
+        <h4>{user.name}</h4>
+        <h4>{user.company}</h4>
+        <a href={user.html_url}>Profile</a>
+      </div>
+    </div>
+  );
 };
 export default MultipleReturnsFetchData;
