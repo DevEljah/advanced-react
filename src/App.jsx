8868ce9e98cbd5f2695f9c2c75d70d2333670e95
Jahid Eljah <jahideljah@gmail.com>
2023-02-10 19:57:45 +0100

    short circuit overview (Falsy and Truthy in jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import Starter from "./tutorial/02-useEffect/starter/04-fetch-data";
 // import Starter from "./tutorial/03-conditional-rendering/starter/01-multiple-returns-basics";
-import Starter from "./tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data";
+import Starter from "./tutorial/03-conditional-rendering/starter/04-short-circuit-overview";
 import Final from "./tutorial/03-conditional-rendering/final/02-multiple-returns-fetch-data";
 
 function App() {
--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,26 @@
-import { useState } from 'react';
+import { useState } from "react";
+import CodeExample from "./../../02-useEffect/final/01-code-example";
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  // falsy
+  const [text, setText] = useState("");
+  // truthy
+  const [name, setName] = useState("Dev");
+
+  const codeExample = name || "Hi";
+  // you go also like this :) ! //
+
+  return (
+    <div>
+      <h2>short circuit overview</h2>
+      {/* {if(someCondition) {"won#t work!"}}
+      we cant use "if" Condition in "jsx"! */}
+      <h4>Falsy "OR" Operator: {text || "Hi"}</h4>
+      <h4>Falsy "AND" Operator: {text && "Hi"}</h4>
+      <h4>Truthy "OR" Operator: {name || "Hi"}</h4>
+      <h4>Truthy "AND" Operator: {name && "Hi"}</h4>
+      <h3>{codeExample} </h3>
+    </div>
+  );
 };
 export default ShortCircuitOverview;
