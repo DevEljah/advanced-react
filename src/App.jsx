731ebacf18706d5f0a4b83465b930b9287b31a09
Jahid Eljah <jahideljah@gmail.com>
2023-02-10 23:05:50 +0100

    short-circuit-example

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import Starter from "./tutorial/02-useEffect/starter/04-fetch-data";
 // import Starter from "./tutorial/03-conditional-rendering/starter/01-multiple-returns-basics";
-import Starter from "./tutorial/03-conditional-rendering/starter/04-short-circuit-overview";
+import Starter from "./tutorial/03-conditional-rendering/starter/05-short-circuit-examples";
 import Final from "./tutorial/03-conditional-rendering/final/02-multiple-returns-fetch-data";
 
 function App() {
