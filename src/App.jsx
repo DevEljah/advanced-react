eef75c7b313fb3140cfe0df854ef0ec6809d2a5f
Jahid Eljah <jahideljah@gmail.com>
2023-02-11 18:43:01 +0100

    cleanup-Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-// import Starter from "./tutorial/02-useEffect/starter/04-fetch-data";
+import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
+import Final from "./tutorial/02-useEffect/final/05-cleanup-function";
 // import Starter from "./tutorial/03-conditional-rendering/starter/01-multiple-returns-basics";
 // import Final from "./tutorial/03-conditional-rendering/final/05-short-circuit-examples";
-import Starter from "./tutorial/03-conditional-rendering/starter/07-user-challenge";
+// import Starter from "./tutorial/03-conditional-rendering/starter/07-user-challenge";
 
 function App() {
   return (
     <div className="container">
       <Starter />
-      {/*    <Final /> */}
+      {/* <Final /> */}
     </div>
   );
 }
--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,44 @@
+import { useState, useEffect } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toogle, setToggle] = useState(false);
+  console.log("render");
+
+  return (
+    <div>
+      <h2>cleanup function</h2>
+      <br />
+      <button onClick={() => setToggle(!toogle)} className="btn">
+        press
+      </button>
+      {toogle && <SecondComponent />}
+    </div>
+  );
+};
+
+const SecondComponent = () => {
+  useEffect(() => {
+    console.log("useEffcted :)");
+    //"initial render" every time you press (mount/unmount)
+    const intId = setInterval(() => {
+      console.log("Hi from Interval");
+    }, 1000);
+    //cleanup-Function
+    return () => {
+      clearInterval(intId);
+      console.log("Clean-up");
+      //the cleanup "func" runs after the "render" and
+      //before the "useEffect" !
+      //basically when the component unmount it runs !!
+    };
+  }, []);
+
+  return (
+    <div>
+      <h4>Second-Component</h4>
+      <h2>Holla!</h2>
+    </div>
+  );
 };
 
 export default CleanupFunction;
