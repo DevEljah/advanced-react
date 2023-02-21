f9e5cf89e9d102cb55cf4eb7aa9241e3a66f9c31
Jahid Eljah <jahideljah@gmail.com>
2023-02-21 22:57:52 +0100

    checkbox & select input

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/06-forms/starter/03-multiple-inputs";
+import Starter from "./tutorial/06-forms/starter/04-other-inputs";
 // import Final from "./tutorial/06-forms/final/02-user-challenge";
 // import Final from "./tutorial/02-useEffect/final/05-cleanup-function";
 
